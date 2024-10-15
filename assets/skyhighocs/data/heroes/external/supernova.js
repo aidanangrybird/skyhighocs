229a08d86c0c0c6dd9c07b348e0fcd5fea056f6e
aidanangrybird <aidanabgaming@gmail.com>
2024-10-15 13:27:02 -0500

    more things were using pryetak stuff instead of correct ones

--- assets/skyhighocs/data/heroes/external/supernova.js
+++ assets/skyhighocs/data/heroes/external/supernova.js
@@ -47,8 +47,8 @@ function init(transer) {
           manager.setData(player, "skyhighheroes:dyn/body_temperature", 0.0);
           manager.setData(player, "skyhighheroes:dyn/predation_timer", 0);
           manager.setData(player, "skyhighheroes:dyn/predation", false);
-          manager.setData(player, "skyhighheroes:dyn/pryetak_timer", 0);
-          manager.setData(player, "skyhighheroes:dyn/pryetak", false);
+          manager.setData(player, "skyhighheroes:dyn/supernova_timer", 0);
+          manager.setData(player, "skyhighheroes:dyn/supernova", false);
           manager.setData(player, "fiskheroes:penetrate_martian_invis", true);
         } else {
           if (PackLoader.getSide() == "CLIENT") {
