33a329570abccd13fca0e886d8262c85867df328
aidanangrybird <aidanabgaming@gmail.com>
2024-10-19 12:25:15 -0500

    Now chase stelar has ability to get pryetak

---
+++ assets/skyhighocs/data/heroes/chase_stelar.js
@@ -0,0 +1,72 @@
+var bodyTemp = implement("skyhighheroes:external/body_temperature");
+var stelar = implement("skyhighheroes:external/stelar");
+var uuid = "4da600b8-582a-4fc3-ac2e-ada03d3e478c";
+var transerSystem = implement("skyhighheroes:external/transer_system");
+var transerMessaging = implement("skyhighheroes:external/transer_messaging");
+var transerBrotherBand = implement("skyhighheroes:external/transer_brotherband");
+var transerContacts = implement("skyhighheroes:external/transer_contacts");
+var transerScanner = implement("skyhighheroes:external/transer_scanner");
+var pryetak = implement("skyhighocs:external/pryetak");
+var transerOS = transerSystem.initTranser([transerMessaging, transerBrotherBand, transerContacts, transerScanner, pryetak]);
+function init(hero) {
+  hero.setAliases("chase_stelar");
+  hero.setName("Chase Stelar");
+  hero.setTier(1);
+  hero.setChestplate("Transer");
+  hero.setTierOverride(entity => 0);
+  hero.setVersion("OC");
+  hero.hide();
+
+  transerOS.keyBinds(hero);
+  transerOS.addPowers(hero);
+
+  bodyTemp.initProfiles(hero);
+
+  hero.addKeyBindFunc("CYCLE_CLOTHES", (player, manager) => stelar.cycleClothes(player, manager), "Change Clothes", 1);
+  hero.addKeyBindFunc("SHIMMER_TOGGLE", (player, manager) => stelar.shimmerToggle(player, manager), "Shimmer Toggle", 1);
+  hero.addKeyBindFunc("VISUALIZER_TOGGLE", (player, manager) => stelar.visualizerToggle(player, manager), "Toggle Visualizer", 2);
+  hero.addKeyBindFunc("HOOD_TOGGLE", (player, manager) => stelar.hoodToggle(player, manager), "Toggle Hood", 2);
+  
+  hero.setDefaultScale(1.0);
+  hero.setAttributeProfile(entity => {
+    if (entity.getData("skyhighheroes:dyn/wave_changing_timer") < 1) {
+    } else {
+      return transerOS.getAttributeProfile(entity);
+    };
+  });
+  hero.setModifierEnabled((entity, modifier) => {
+    if (modifier.name() == "fiskheroes:shape_shifting") {
+      return true;
+    };
+    return transerOS.isModifierEnabled(entity, modifier);
+  });
+  hero.setTierOverride(entity => transerOS.getTierOverride(entity));
+  hero.setKeyBindEnabled((entity, keyBind) => {
+    if (keyBind == "VISUALIZER_TOGGLE") {
+      return entity.getData("skyhighheroes:dyn/wave_changing_timer") == 0 && entity.getUUID() == uuid && ((entity.getData("skyhighheroes:dyn/stelar_clothes") == 3) ? !entity.isSneaking() : true);
+    };
+    if (keyBind == "CYCLE_CLOTHES") {
+      return entity.getData("skyhighheroes:dyn/wave_changing_timer") == 0 && !entity.isSneaking() && entity.getUUID() == uuid;
+    };
+    if (keyBind == "SHIMMER_TOGGLE") {
+      return entity.getData("skyhighheroes:dyn/wave_changing_timer") == 0 && entity.isSneaking() && entity.getUUID() == uuid;
+    };
+    if (keyBind == "HOOD_TOGGLE") {
+      return entity.getData("skyhighheroes:dyn/wave_changing_timer") == 0 && entity.getUUID() == uuid && entity.isSneaking() && entity.getData("skyhighheroes:dyn/stelar_clothes") == 3;
+    };
+    if (keyBind == "CYCLE_CHATS") {
+      return !entity.isSneaking() && entity.getData("skyhighheroes:dyn/battle_card") == 0 && entity.getData("skyhighheroes:dyn/wave_changing_timer") == 0;
+    };
+    if (keyBind == "CYCLE_CHAT_MODES") {
+      return entity.isSneaking() && entity.getData("skyhighheroes:dyn/battle_card") == 0 && entity.getData("skyhighheroes:dyn/wave_changing_timer") == 0;
+    };
+    if (keyBind == "SHAPE_SHIFT") {
+      return entity.getData("skyhighheroes:dyn/battle_card") == 0 && ((entity.getData("skyhighheroes:dyn/wave_changing_timer") == 0) ? true : entity.getData("skyhighocs:dyn/jet_streak_timer") == 1);
+    };
+    return transerOS.isKeyBindEnabled(entity, keyBind);
+  });
+  hero.setTickHandler((entity, manager) => {
+    transerOS.tickHandler(entity, manager);
+    bodyTemp.change(entity, manager, stelar.tempProfiles(), "skyhighheroes:dyn/body_temperature", 400.0, "skyhighheroes:dyn/stelar_clothes");
+  });
+};
---
+++ assets/skyhighocs/renderers/heroes/chase_stelar.js
@@ -0,0 +1,22 @@
+extend("skyhighocs:base_stelar");
+
+loadTextures({
+  "transer": "skyhighocs:chase/chase_stelar_transer.tx.json",
+  "visualizer_lights": "skyhighocs:chase/chase_stelar_visualizer_lights.tx.json",
+  "transer_default": "skyhighocs:chase/chase_stelar_transer",
+  "transer_default_lights": "skyhighocs:chase/chase_stelar_transer_lights",
+  "base": "skyhighocs:chase/chase_stelar_base",
+  "lights": "skyhighocs:chase/chase_stelar_lights",
+});
+
+function getColor() {
+  return 0x44FF00;
+};
+
+function getID() {
+  return "4da600b8-582a-4fc3-ac2e-ada03d3e478c";
+};
+
+function getSatellite() {
+  return "leo";
+};
