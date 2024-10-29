841635d1513c1869eeb00ce208e3691f69e42f7d
aidanangrybird <aidanabgaming@gmail.com>
2024-10-28 20:03:45 -0500

    Updated how pryetak nebula is obtained

--- assets/skyhighocs/data/heroes/external/cold_snow.js
+++ assets/skyhighocs/data/heroes/external/cold_snow.js
@@ -3,7 +3,22 @@ function init(transer) {
     name: "Death by snow",
     type: 7,
     human: "Chase Stelar",
-    waveHandler: function (entity, manager) {
+    selfProfile: function (hero) {
+      hero.addDamageProfile("SELF", {
+        "types": {
+          "WAVE_COLD": 1.0,
+          "WAVE_ICE": 1.0
+        }
+      });
+    },
+    otherProfile: function (hero) {
+      hero.addDamageProfile("OTHER", {
+        "types": {
+          "WAVE_ENERGY": 1.0
+        }
+      });
+    },
+    waveCalling: function (entity, manager) {
       if (entity.getUUID() == "4da600b8-582a-4fc3-ac2e-ada03d3e478c" && entity.world().isUnobstructed(entity.eyePos(), entity.eyePos().add(0,1000,0)) && !entity.world().isRaining() && !entity.world().isThundering() && entity.world().getLocation(entity.pos()).biome().startsWith("Cold Taiga Hills")) {
         var value = Math.random();
         if (value < 0.01) {
@@ -16,5 +31,18 @@ function init(transer) {
         };
       };
     },
+    waveHandler: function (entity, hero) {
+      if (entity.getData("skyhighheroes:dyn/calling_timer") > 0.3 && entity.getData("skyhighheroes:dyn/calling_timer") < 0.7) {
+        var nearbyPlayers = entity.world().getEntitiesInRangeOf(entity.pos(), 10);
+        nearbyPlayers.forEach(other => {
+          if (other.getName() != entity.getName()) {
+            other.hurt(hero, "OTHER", "%1$s died by Pryetak", 10.0);
+          };
+        });
+      };
+      if (entity.getData("skyhighheroes:dyn/calling_timer") > 0.425 && entity.getData("skyhighheroes:dyn/calling_timer") < 0.475) {
+        entity.hurt(hero, "SELF", "%1$s could not handle wave changing", 1.0);
+      };
+    }
   }
 }
