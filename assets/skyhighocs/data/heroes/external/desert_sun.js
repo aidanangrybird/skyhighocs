98957db1614bbf68c7f5ea58abe03b3ea0a374ba
aidanangrybird <aidanabgaming@gmail.com>
2024-10-28 20:21:41 -0500

    Updated how solar flame is obtained

--- assets/skyhighocs/data/heroes/external/desert_sun.js
+++ assets/skyhighocs/data/heroes/external/desert_sun.js
@@ -3,6 +3,21 @@ function init(transer) {
     name: "Burning man",
     type: 7,
     human: "Ace Stelar",
+    selfProfile: function (hero) {
+      hero.addDamageProfile("SELF", {
+        "types": {
+          "WAVE_FIRE": 1.0,
+          "WAVE_LIGHT": 1.0
+        }
+      });
+    },
+    otherProfile: function (hero) {
+      hero.addDamageProfile("OTHER", {
+        "types": {
+          "WAVE_ENERGY": 1.0
+        }
+      });
+    },
     waveHandler: function (entity, manager) {
       if (entity.getUUID() == "87fa6187-4fa6-4dc6-8742-19a2b67c4cc0" && entity.world().isUnobstructed(entity.eyePos(), entity.eyePos().add(0,1000,0)) && !entity.world().isRaining() && !entity.world().isThundering() && entity.world().getLocation(entity.pos()).biome().startsWith("Desert")) {
         var value = Math.random();
@@ -16,5 +31,18 @@ function init(transer) {
         };
       };
     },
+    waveHandler: function (entity, hero) {
+      if (entity.getData("skyhighheroes:dyn/calling_timer") > 0.3 && entity.getData("skyhighheroes:dyn/calling_timer") < 0.7) {
+        var nearbyPlayers = entity.world().getEntitiesInRangeOf(entity.pos(), 10);
+        nearbyPlayers.forEach(other => {
+          if (other.getName() != entity.getName()) {
+            other.hurt(hero, "OTHER", "%1$s died by Solar", 10.0);
+          };
+        });
+      };
+      if (entity.getData("skyhighheroes:dyn/calling_timer") > 0.425 && entity.getData("skyhighheroes:dyn/calling_timer") < 0.475) {
+        entity.hurt(hero, "SELF", "%1$s could not handle wave changing", 1.0);
+      };
+    }
   }
 }
