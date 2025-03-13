253276e22a4f959d20ad82f4e3cede26e2086adc
aidanangrybird <aidanabgaming@gmail.com>
2025-03-13 12:51:58 -0500

    Shifted base powers to start of power adding function so they will always be added

--- assets/skyhighocs/data/heroes/external/cyber_system.js
+++ assets/skyhighocs/data/heroes/external/cyber_system.js
@@ -293,7 +293,7 @@ function initSystem(moduleList, name, normalName, color) {
   /** @var damageProfileIndexes - Indexes of damage profile capable modules */
   var damageProfileIndexes = [];
   /** @var powerArray - Array of powers to add */
-  var powerArray = ["skyhighocs:cyber_system", "skyhighocs:cyber_body"];
+  var powerArray = [];
   /** @var disguisedName - disguised name */
   var disguisedName = normalName;
   /** @var cyberName - cyber name */
@@ -550,35 +550,38 @@ function initSystem(moduleList, name, normalName, color) {
      * @param {JSHero} hero - Required
      **/
     addPowers: (hero) => {
+      if (powerArray.length == 0) {
+        hero.addPowers("skyhighocs:cyber_system", "skyhighocs:cyber_body");
+      };
       if (powerArray.length == 1) {
-        hero.addPowers(powerArray[0]);
+        hero.addPowers("skyhighocs:cyber_system", "skyhighocs:cyber_body", powerArray[0]);
       };
       if (powerArray.length == 2) {
-        hero.addPowers(powerArray[0], powerArray[1]);
+        hero.addPowers("skyhighocs:cyber_system", "skyhighocs:cyber_body", powerArray[0], powerArray[1]);
       };
       if (powerArray.length == 3) {
-        hero.addPowers(powerArray[0], powerArray[1], powerArray[2]);
+        hero.addPowers("skyhighocs:cyber_system", "skyhighocs:cyber_body", powerArray[0], powerArray[1], powerArray[2]);
       };
       if (powerArray.length == 4) {
-        hero.addPowers(powerArray[0], powerArray[1], powerArray[2], powerArray[3]);
+        hero.addPowers("skyhighocs:cyber_system", "skyhighocs:cyber_body", powerArray[0], powerArray[1], powerArray[2], powerArray[3]);
       };
       if (powerArray.length == 5) {
-        hero.addPowers(powerArray[0], powerArray[1], powerArray[2], powerArray[3], powerArray[4]);
+        hero.addPowers("skyhighocs:cyber_system", "skyhighocs:cyber_body", powerArray[0], powerArray[1], powerArray[2], powerArray[3], powerArray[4]);
       };
       if (powerArray.length == 6) {
-        hero.addPowers(powerArray[0], powerArray[1], powerArray[2], powerArray[3], powerArray[4], powerArray[5]);
+        hero.addPowers("skyhighocs:cyber_system", "skyhighocs:cyber_body", powerArray[0], powerArray[1], powerArray[2], powerArray[3], powerArray[4], powerArray[5]);
       };
       if (powerArray.length == 7) {
-        hero.addPowers(powerArray[0], powerArray[1], powerArray[2], powerArray[3], powerArray[4], powerArray[5], powerArray[6]);
+        hero.addPowers("skyhighocs:cyber_system", "skyhighocs:cyber_body", powerArray[0], powerArray[1], powerArray[2], powerArray[3], powerArray[4], powerArray[5], powerArray[6]);
       };
       if (powerArray.length == 8) {
-        hero.addPowers(powerArray[0], powerArray[1], powerArray[2], powerArray[3], powerArray[4], powerArray[5], powerArray[6], powerArray[7]);
+        hero.addPowers("skyhighocs:cyber_system", "skyhighocs:cyber_body", powerArray[0], powerArray[1], powerArray[2], powerArray[3], powerArray[4], powerArray[5], powerArray[6], powerArray[7]);
       };
       if (powerArray.length == 9) {
-        hero.addPowers(powerArray[0], powerArray[1], powerArray[2], powerArray[3], powerArray[4], powerArray[5], powerArray[6], powerArray[7], powerArray[8]);
+        hero.addPowers("skyhighocs:cyber_system", "skyhighocs:cyber_body", powerArray[0], powerArray[1], powerArray[2], powerArray[3], powerArray[4], powerArray[5], powerArray[6], powerArray[7], powerArray[8]);
       };
       if (powerArray.length == 10) {
-        hero.addPowers(powerArray[0], powerArray[1], powerArray[2], powerArray[3], powerArray[4], powerArray[5], powerArray[6], powerArray[7], powerArray[8], powerArray[9]);
+        hero.addPowers("skyhighocs:cyber_system", "skyhighocs:cyber_body", powerArray[0], powerArray[1], powerArray[2], powerArray[3], powerArray[4], powerArray[5], powerArray[6], powerArray[7], powerArray[8], powerArray[9]);
       };
     },
     /**
