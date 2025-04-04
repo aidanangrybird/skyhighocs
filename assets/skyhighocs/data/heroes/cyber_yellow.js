8a7cf139d65f51aa8c77c7d50513380eca14cc7d
aidanangrybird <aidanabgaming@gmail.com>
2025-04-04 00:21:04 -0500

    Added Cyber Yellow

---
+++ assets/skyhighocs/data/heroes/cyber_yellow.js
@@ -0,0 +1,43 @@
+var uuid = "a3d071d4-c912-41e1-a6b2-c0de99ea4a84";
+var cybernetics = implement("skyhighocs:external/cybernetics");
+var settings = implement("skyhighocs:external/settings");
+var messaging = implement("skyhighheroes:external/messaging");
+var groupMessaging = implement("skyhighheroes:external/group_messaging");
+var groups = implement("skyhighheroes:external/groups");
+var contacts = implement("skyhighheroes:external/contacts");
+var scanner = implement("skyhighheroes:external/scanner");
+var waypoints = implement("skyhighheroes:external/waypoint");
+var communications = implement("skyhighocs:external/communications");
+var cyberneticMessaging = implement("skyhighocs:external/cybernetic_messaging");
+var cannons = implement("skyhighocs:external/cannons");
+var rockets = implement("skyhighocs:external/rockets");
+var blades = implement("skyhighocs:external/blades");
+var shields = implement("skyhighocs:external/shields");
+var thermopticManipulation = implement("skyhighocs:external/thermoptic_manipulation");
+var wings = implement("skyhighocs:external/wings");
+var externalArms = implement("skyhighocs:external/external_arms");
+var suitDatastore = implement("skyhighocs:external/suit_datastore");
+var voiceSynthesizer = implement("skyhighocs:external/voice_synthesizer");
+var cyberOS = cybernetics.initSystem([
+  settings,
+  messaging,
+  groupMessaging,
+  groups,
+  contacts,
+  scanner,
+  waypoints,
+  communications,
+  cyberneticMessaging,
+  cannons,
+  rockets,
+  blades,
+  shields,
+  thermopticManipulation,
+  wings,
+  externalArms,
+  suitDatastore,
+  voiceSynthesizer,
+], "Cyber Yellow", "yelloww69", "3", uuid);
+function init(hero) {
+  cyberOS.initCybernetics(hero);
+};
--- assets/skyhighocs/data/heroes/external/cybernetics.js
+++ assets/skyhighocs/data/heroes/external/cybernetics.js
@@ -39,7 +39,7 @@ var colorDamage = {
   //Red
   "4": "1",
   //Cyan, figure this out
-  "3": "8"
+  "3": "6"
 };
 
 modelRegex = /[a-z\s]/gm;
