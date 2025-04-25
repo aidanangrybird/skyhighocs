7bdf015add1a2258d4cd8b395b9101eaf4973b8c
aidanangrybird <aidanabgaming@gmail.com>
2025-04-25 18:47:53 -0500

    Changed deploy and retract to show and hide

--- assets/skyhighocs/data/heroes/external/communications.js
+++ assets/skyhighocs/data/heroes/external/communications.js
@@ -339,7 +339,7 @@ function initModule(system) {
               });
             };
             break;
-          case "deploy":
+          case "show":
             switch (arguments[2]) {
               case "sat":
                 manager.setData(entity, "skyhighocs:dyn/satellite", true);
@@ -355,7 +355,7 @@ function initModule(system) {
                 break;
             };
             break;
-          case "retract":
+          case "hide":
             switch (arguments[2]) {
               case "sat":
                 manager.setData(entity, "skyhighocs:dyn/satellite", false);
@@ -379,8 +379,8 @@ function initModule(system) {
             break;
           case "help":
             system.moduleMessage(this, entity, "<n>Communications commands:");
-            system.moduleMessage(this, entity, "<n>!comms deploy <sat|ant|satRain> <nh>-<n> Deploys comms type");
-            system.moduleMessage(this, entity, "<n>!comms retract <sat|ant|satRain> <nh>-<n> Retracts comms type");
+            system.moduleMessage(this, entity, "<n>!comms show <sat|ant|satRain> <nh>-<n> Deploys comms type");
+            system.moduleMessage(this, entity, "<n>!comms hide <sat|ant|satRain> <nh>-<n> Retracts comms type");
             system.moduleMessage(this, entity, "<n>!comms suits <suits> <nh>-<n> Transmits suits (comma seperated indexes) to other Cybers");
             system.moduleMessage(this, entity, "<n>!comms cyberPos <nh>-<n> Gets position of other Cybers");
             system.moduleMessage(this, entity, "<n>!comms cyberStatus <nh>-<n> Gets stats of other Cybers");
