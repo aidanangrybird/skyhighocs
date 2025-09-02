091a009567396a01327e693d01d92b2eca2242b1
aidanangrybird <aidanabgaming@gmail.com>
2025-09-02 14:45:38 -0500

    Removed all of pezzos stuff because he is addicted to thematic

--- assets/skyhighocs/data/heroes/lucas_the_invincible.js
+++
@@ -1,5 +0,0 @@
-var chakraSuit = implement("skyhighocs:external/chakra_suit");
-var uuid = "c4bc5db6-3cf6-44fe-8427-304a7b211bc4";
-function init(hero) {
-  chakraSuit.initChakras(hero, "Lucas", uuid);
-};
--- assets/skyhighocs/renderers/heroes/artemis_metal.js
+++
@@ -1,8 +0,0 @@
-extend("skyhighocs:artemis");
-
-var astro = implement("skyhighocs:external/astro");
-var stuff = implement("skyhighocs:external/stuff");
-
-loadTextures({
-  "full": "skyhighocs:lucas/artemis_full_base_metal",
-});
