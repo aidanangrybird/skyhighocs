692b39b9856cf5cb14ac63993dde330556e4adf1
aidanangrybird <aidanabgaming@gmail.com>
2026-03-23 13:48:10 -0500

    Updated wording of panel opener and closer feedback

--- assets/skyhighocs/data/heroes/external/astro_opener.js
+++ assets/skyhighocs/data/heroes/external/astro_opener.js
@@ -59,12 +59,12 @@ function initModule(system) {
           case "torso":
             manager.setData(entity, "skyhighocs:dyn/torso_front_open", true);
             manager.setData(entity, "skyhighocs:dyn/torso_back_open", true);
-            system.moduleMessage(this, entity, "<n>Opening upper body!");
+            system.moduleMessage(this, entity, "<n>Opening torso!");
             break;
           case "shorts":
             manager.setData(entity, "skyhighocs:dyn/shorts_front_open", true);
             manager.setData(entity, "skyhighocs:dyn/shorts_back_open", true);
-            system.moduleMessage(this, entity, "<n>Opening lower body!");
+            system.moduleMessage(this, entity, "<n>Opening shorts!");
             break;
           case "bodyFront":
             manager.setData(entity, "skyhighocs:dyn/torso_front_open", true);
@@ -78,19 +78,19 @@ function initModule(system) {
             break;
           case "torsoFront":
             manager.setData(entity, "skyhighocs:dyn/torso_front_open", true);
-            system.moduleMessage(this, entity, "<n>Opening upper front body!");
+            system.moduleMessage(this, entity, "<n>Opening front torso!");
             break;
           case "shortsFront":
             manager.setData(entity, "skyhighocs:dyn/shorts_front_open", true);
-            system.moduleMessage(this, entity, "<n>Opening lower front body!");
+            system.moduleMessage(this, entity, "<n>Opening front shorts!");
             break;
           case "torsoBack":
             manager.setData(entity, "skyhighocs:dyn/torso_back_open", true);
-            system.moduleMessage(this, entity, "<n>Opening upper back body!");
+            system.moduleMessage(this, entity, "<n>Opening back torso!");
             break;
           case "shortsBack":
             manager.setData(entity, "skyhighocs:dyn/shorts_back_open", true);
-            system.moduleMessage(this, entity, "<n>Opening lower back body!");
+            system.moduleMessage(this, entity, "<n>Opening back shorts!");
             break;
           case "core":
             manager.setData(entity, "skyhighocs:dyn/core_open", true);
